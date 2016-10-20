1bbbafa2d41e928a4f0f31c8a385f3659b6f0d48
Jovi Chan <czwjovi@foxmail.com>
2016-10-20 04:20:25 -0700

    demo

---
+++ app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent}   from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+		AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+		HeroDetailComponent,
+		HeroesComponent,
+		DashboardComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { 
+}
+
